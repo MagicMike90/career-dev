f3f47867b508ae3831e2a11193025cca94352a6d
Michael Luo <mluo@echoice.com.au>
2018-05-09 16:00:13 +1000

    enable nginx

--- nginx/local_cert_gen.sh
+++ nginx/local_cert_gen.sh
@@ -1,1 +1,2 @@
-openssl genrsa -des3 -out rootCA.key 2048
+openssl req -x509 -sha256 -nodes -newkey rsa:2048 -days 365 -keyout ./ssl/certs/localhost.key -out ./ssl/certs/localhost.crt
+sudo security add-trusted-cert -d -r trustRoot -k /Library/Keychains/System.keychain ./ssl/certs/localhost.crt
